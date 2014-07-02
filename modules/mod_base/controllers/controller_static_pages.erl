williamthome/zotonic

commit cb1f312b64b9a9badd78131457fdfd7e4e922011

core: introducing lager metadata for better logging. Added the session_id to the #context{} for better debugging/logging. Misc fixes for authentication. Issue #781

modules/mod_base/controllers/controller_static_pages.erl
@@ -19,7 +19,9 @@
 
 -module(controller_static_pages).
 -export([init/1]).
--export([allowed_methods/2,
+-export([
+     service_available/2,
+     allowed_methods/2,
 	 resource_exists/2,
 	 last_modified/2,
 	 expires/2,
@@ -66,6 +68,11 @@ init(ConfigProps) ->
     DirIndex = z_convert:to_bool(proplists:get_value(allow_directory_index, ConfigProps)),
     UseCache = proplists:get_value(use_cache, ConfigProps, false),
     {ok, #state{root=Root, use_cache=UseCache, config=ConfigProps, allow_directory_index=DirIndex}}.
+
+
+service_available(ReqData, State) ->
+    z_context:lager_md([{controller, ?MODULE}], ReqData),
+    {true, State}.
     
 allowed_methods(ReqData, State) ->
     {['HEAD', 'GET'], ReqData, State}.

include/controller_html_helper.hrl
@@ -36,6 +36,7 @@ init(DispatchArgs) ->
 service_available(ReqData, DispatchArgs) when is_list(DispatchArgs) ->
     Context  = z_context:new(ReqData, ?MODULE),
     Context1 = z_context:set(DispatchArgs, Context),
+    z_context:lager_md(Context1),
     ?WM_REPLY(true, Context1).
 
 charsets_provided(ReqData, Context) ->
@@ -44,5 +45,6 @@ charsets_provided(ReqData, Context) ->
 to_html(ReqData, Context) ->
     Context1 = ?WM_REQ(ReqData, Context),
     Context2 = z_context:ensure_all(Context1),
+    z_context:lager_md(Context2),
     {Result, ResultContext} = html(Context2),
     ?WM_REPLY(Result, ResultContext).

include/zotonic.hrl
@@ -26,7 +26,7 @@
 %% @doc The request context, session information and other
 -record(context, {
         %% The host
-        host=default,
+        host=default :: atom(),
 
         %% Webmachine request data (only set when this context is used because of a request)
         wm_reqdata=undefined :: #wm_reqdata{} | undefined,
@@ -36,6 +36,7 @@
         
         %% The page and session processes associated with the current request
         session_pid=undefined :: pid() | undefined,  % one session per browser (also manages the persistent data)
+        session_id=undefined :: string() | undefined,
         page_pid=undefined :: pid() | undefined,     % multiple pages per session, used for pushing information to the browser
         page_id=undefined :: string() | undefined,
 

modules/mod_admin/controllers/controller_admin.erl
@@ -25,7 +25,10 @@
 -include_lib("controller_html_helper.hrl").
 
 is_authorized(ReqData, Context) ->
-    z_acl:wm_is_authorized(use, z_context:get(acl_module, Context, mod_admin), admin_logon, ReqData, Context).
+	Context1 = ?WM_REQ(ReqData, Context),
+	Context2 = z_context:ensure_all(Context1),
+	z_context:lager_md(Context2),
+    z_acl:wm_is_authorized([{use, z_context:get(acl_module, Context, mod_admin)}], admin_logon, Context2).
 
 html(Context) ->
     Template = z_context:get(template, Context, "admin.tpl"),

modules/mod_admin/controllers/controller_admin_edit.erl
@@ -62,6 +62,7 @@ ensure_id(Context) ->
             {Context2, N};
         undefined ->
             try
+                z_context:lager_md(Context2),
                 {ok, IdN} = m_rsc:name_to_id(z_context:get_q("id", Context2), Context2),
                 {z_context:set(id, IdN, Context2), IdN}
             catch

modules/mod_admin/controllers/controller_admin_media_preview.erl
@@ -34,11 +34,13 @@ init(_Args) -> {ok, []}.
 service_available(ReqData, DispatchArgs) when is_list(DispatchArgs) ->
     Context  = z_context:new(ReqData, ?MODULE),
     Context1 = z_context:set(DispatchArgs, Context),
+    z_context:lager_md(Context1),
     ?WM_REPLY(true, Context1).
 
 resource_exists(ReqData, Context0) ->
     ContextReq = ?WM_REQ(ReqData, Context0),
     Context = z_context:ensure_all(ContextReq), 
+    z_context:lager_md(Context),
     case z_context:get_q("id", Context) of
         undefined ->
             ?WM_REPLY(false, Context);

modules/mod_admin/controllers/controller_admin_referrers.erl
@@ -27,19 +27,16 @@
 -include_lib("controller_html_helper.hrl").
 
 is_authorized(ReqData, Context) ->
-    z_acl:wm_is_authorized(use, mod_admin, admin_logon, ReqData, Context).
+    Context1 = ?WM_REQ(ReqData, Context),
+    Context2 = z_context:ensure_all(Context1),
+    z_context:lager_md(Context2), 
+    z_acl:wm_is_authorized([{use, mod_admin}], admin_logon, Context2).
 
 resource_exists(ReqData, Context) ->
     Context1 = ?WM_REQ(ReqData, Context),
-    Context2 = z_context:ensure_all(Context1),
-    Id = z_context:get_q("id", Context2),
-    try
-        IdN = list_to_integer(Id),
-        Context3 = z_context:set(id, IdN, Context2),
-        ?WM_REPLY(m_rsc:exists(IdN, Context3), Context3)
-    catch
-        _:_ -> ?WM_REPLY(false, Context2)
-    end.
+    Id = m_rsc:rid(z_context:get_q("id", Context1), Context1),
+    Context2 = z_context:set(id, Id, Context1),
+    ?WM_REPLY(m_rsc:exists(Id, Context2), Context2).
 
 html(Context) ->
     Vars = [

modules/mod_base/controllers/controller_api.erl
@@ -41,12 +41,14 @@ init(DispatchArgs) -> {ok, DispatchArgs}.
 service_available(ReqData, DispatchArgs) when is_list(DispatchArgs) ->
     Context  = z_context:new(ReqData, ?MODULE),
     Context1 = z_context:set(DispatchArgs, Context),
+    z_context:lager_md(Context1),
     ?WM_REPLY(true, Context1).
 
 
 allowed_methods(ReqData, Context) ->
     Context0 = ?WM_REQ(ReqData, Context),
     Context1 = z_context:ensure_qs(z_context:continue_session(Context0)),
+    z_context:lager_md(Context1),
 
     TheMod = case z_context:get_q("module", Context1) of
                  undefined -> z_convert:to_list(z_context:get(module, Context1));

commit d9bd324522709637c9b261b1d10d08ede374419f

mod_base: Fix service_available/2 in controller_static_pages
cb1f312b64b9a9badd78131457fdfd7e4e922011 introduced a wrong webmachine
reply

modules/mod_base/controllers/controller_static_pages.erl
@@ -72,7 +72,7 @@ init(ConfigProps) ->
 
 service_available(ReqData, State) ->
     z_context:lager_md([{controller, ?MODULE}], ReqData),
-    {true, State}.
+    {true, ReqData, State}.
     
 allowed_methods(ReqData, State) ->
     {['HEAD', 'GET'], ReqData, State}.