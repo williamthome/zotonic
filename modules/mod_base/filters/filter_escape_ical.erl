f714b42287134d892aabea99d7ad987c33199ef1
Marc Worrell <marc@worrell.nl>
2020-11-17 16:11:31 +0100

    Change ical escape of ":"

--- modules/mod_base/filters/filter_escape_ical.erl
+++ modules/mod_base/filters/filter_escape_ical.erl
@@ -23,4 +23,23 @@
 escape_ical(undefined, _Context) ->
 	<<>>;
 escape_ical(In, _Context) ->
-	z_string:escape_ical(In).
+	escape_ical(In).
+
+escape_ical(L) when is_list(L) ->
+    escape_ical(iolist_to_binary(L));
+escape_ical(B) when is_binary(B) ->
+    escape_ical(B, <<>>, 0);
+escape_ical(A) when is_atom(A) ->
+    escape_ical(atom_to_list(A)).
+
+escape_ical(<<>>, Acc, _N) -> Acc;
+escape_ical(B, Acc, N) when N >= 70 -> escape_ical(B, <<Acc/binary, 13, 10, 32>>, 0);
+escape_ical(<<13, 10, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, $\\, $n>>, N+2);
+escape_ical(<<10, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, $\\, $n>>, N+2);
+escape_ical(<<9, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, 32>>, N+1);
+escape_ical(<<$", Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, $\\, $">>, N+2);
+escape_ical(<<$,, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, $\\, $,>>, N+2);
+escape_ical(<<$:, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, $\\, $:>>, N+3);
+escape_ical(<<$;, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, $\\, $;>>, N+2);
+escape_ical(<<$\\, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, $\\, $\\>>, N+2);
+escape_ical(<<C, Rest/binary>>, Acc, N) -> escape_ical(Rest, <<Acc/binary, C>>, N+1).
